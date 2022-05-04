3b6ffe98918b31a8ce3fbc45bb0081e0b173ab67
Darío <me@dariodessaunet.dev>
2022-05-04 10:34:15 -0300

    add linter prettier checks (#2)
    * Add linter and prettier checks
    
    * Remove yarn usage over npm

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'array-callback-return': 'error',
+    'no-template-curly-in-string': 'off',
+    'prettier/prettier': ['error', prettierOptions],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+};
